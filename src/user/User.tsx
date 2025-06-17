f583cc33c70bf5fa6e905c182a4b3e53b885971a
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2025-06-17 15:41:03 -0300

    New design in user detail view

--- src/user/User.tsx
+++ src/user/User.tsx
@@ -1,19 +1,33 @@
 import { useUser } from "../contexts/UserContext";
+import { Card, Row, Col } from "react-bootstrap";
+
 export default function User() {
   const { user } = useUser();
 
   return (
     <div>
       <h2 className="mb-4">Detalle del Usuario</h2>
-      {user && (
-        <div>
-          <p>
-            <strong>Nombre:</strong> {user.user_metadata?.username}
-          </p>
-          <p>
-            <strong>Email:</strong> {user.email}
-          </p>
-        </div>
+      {user ? (
+        <Card>
+          <Card.Body>
+            <Row>
+              <Col md={3} className="fw-semibold">
+                Nombre:
+              </Col>
+              <Col md={9}>
+                {user.user_metadata?.username || "No disponible"}
+              </Col>
+            </Row>
+            <Row className="mt-3">
+              <Col md={3} className="fw-semibold">
+                Email:
+              </Col>
+              <Col md={9}>{user.email || "No disponible"}</Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      ) : (
+        <p>No se encontró información del usuario.</p>
       )}
     </div>
   );
